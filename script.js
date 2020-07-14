b56fb4d0bf758f26af04f4b24401788497636a7d
kellynym <kellynym@gmail.com>
2020-07-13 21:44:47 -0400

    Bootstrap & JS/jQuery Worksheet

---
+++ script.js
@@ -0,0 +1,46 @@
+$(".1").click(function() {
+$(".firstDiv").addClass("firstDivnew")
+})
+
+$(".2").click(function() {
+    $(".secondDiv").addClass("secondDivnew")
+    })
+
+    $(".3").click(function() {
+        $(".thirdDiv").addClass("thirdDivnew")
+        })    
+
+
+$(".4").click(function() {
+    $(".firstDiv").addClass("firstDivnew");
+    $(".secondDiv").addClass("secondDivnew");
+    $(".thirdDiv").addClass("thirdDivnew") })   
+
+
+    $(".5").click(function() {
+        $(".firstDiv").hide("1000")
+        })    
+
+    $(".6").click(function() {
+            $(".secondDiv").hide("1000")
+            })   
+
+     $(".7").click(function() {
+        $(".thirdDiv").hide("1000")
+        })           
+
+        $(".8").click(function() {
+            $(".firstDiv").hide("1000");
+            $(".secondDiv").hide("1000");
+            $(".thirdDiv").hide("1000") }) 
+
+    $(".9").click(function(){
+    $(".firstDiv").removeClass("firstDivnew");
+    $(".secondDiv").removeClass("secondDivnew");
+    $(".thirdDiv").removeClass("thirdDivnew")})
+           
+ $(".10").click(function(){
+  $(".firstDiv, .secondDiv, .thirdDiv").show("1000")
+     })
+
+ 
